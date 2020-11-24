e44d0577aa428873558c8059869dc50fce8a683b
cleisonsantos <css.cleison@outlook.com>
2020-11-23 23:08:25 -0300

    criação do projeto e configs iniciais.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div >
+      <header >
+        <img src={logo} alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
